34d5151771ef62dbef89ea7a4bdbcfec5b927378
Yarin Levi <85311237+yarinl139@users.noreply.github.com>
2022-12-31 19:12:06 +0200

    Add files via upload

--- isort.c
+++ isort.c
@@ -12,13 +12,13 @@ void shift_element(int* arr, int i)
 void insertion_sort(int array[], int len) {
     int i, j, val;
     for (i = 1; i < len; i++) {
-        val = array[i];
+        val = *(array + i);
         j = i - 1;
-        while (j >= 0 && array[j] > val) {
-            array[j + 1] = array[j];
+        while (j >= 0 && *(array +j) > val) {
+            *(array +j+1) = *(array +j);
             j = j - 1;
         }
-        array[j + 1] = val;
+         *(array +j+1) = val;
     }
 }
 int main()
@@ -26,17 +26,17 @@ int main()
     int arr[SIZE];
     for (int i = 0; i < SIZE; i++)
     {
-        scanf(" %d",&arr[i]);
+        scanf(" %d",&(*(arr+i)));
     }
     insertion_sort(arr,SIZE);
     for (int i = 0; i < SIZE; i++)
     {
         if(i==SIZE-1)
         {
-          printf("%d",arr[i]);
+          printf("%d",*(arr+i));
           break;
         }
-        printf("%d,",arr[i]);
+        printf("%d,",*(arr+i));
     }
     printf("\n");
 
